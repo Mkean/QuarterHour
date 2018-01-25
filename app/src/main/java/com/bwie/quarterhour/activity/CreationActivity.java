0290ea095f82d19e84a83c1c9f0c64b84e6dd82d
子非鱼 <3162562449@qq.com>
2018-01-25 14:52:44 +0800

    First commit

--- app/src/main/java/com/bwie/quarterhour/activity/CreationActivity.java
+++ app/src/main/java/com/bwie/quarterhour/activity/CreationActivity.java
@@ -1,19 +1,31 @@
 package com.bwie.quarterhour.activity;
 
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bwie.quarterhour.R;
 
 public class CreationActivity extends BaseActivity {
 
+    private LinearLayout mVideo;
+    private TextView mRight;
+    private TextView mTitle;
+    private TextView mLeft;
+    private LinearLayout mJokes;
+
     @Override
     protected void initData() {
-
+        mRight.setVisibility(View.GONE);
     }
 
     @Override
     protected void initView() {
-
+        mVideo = (LinearLayout) findViewById(R.id.create_video);
+        mJokes = (LinearLayout) findViewById(R.id.create_jokes);
+        mLeft = (TextView) findViewById(R.id.text_left);
+        mTitle = (TextView) findViewById(R.id.text_title);
+        mRight = (TextView) findViewById(R.id.text_right);
     }
 
     @Override
@@ -23,11 +35,26 @@ public class CreationActivity extends BaseActivity {
 
     @Override
     protected void setListener() {
-
+        mVideo.setOnClickListener(this);
+        mJokes.setOnClickListener(this);
+        mLeft.setOnClickListener(this);
     }
 
     @Override
     public void widgetClick(View v) {
+        switch (v.getId()) {
+            case R.id.create_video:
 
+                break;
+            case R.id.create_jokes:
+                $startActivity(JokesActivity.class);
+                finish();
+                break;
+
+            case R.id.text_left:
+                finish();
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/bwie/quarterhour/activity/JokesActivity.java
@@ -0,0 +1,15 @@
+package com.bwie.quarterhour.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwie.quarterhour.R;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+    }
+}
